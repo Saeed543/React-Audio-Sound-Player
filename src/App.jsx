b18feff2f2200adbeb276ac6959a63c7ed95e5b6
Safiullah <safiullahsaleem820@gmail.com>
2024-02-18 19:14:12 +0500

    First Commit �

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="bg-[#16132a] min-h-screen bg-treebackground bg-contain bg-no-repeat h-screen bg-center ">App</div>
+  )
+}
+
+export default App;
